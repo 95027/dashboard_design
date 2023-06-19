ce361f4612ee27a093f0ca6bb5b19a6ea5f49e67
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-06-19 23:38:57 +0530

    Update Routings.js

--- src/routings/Routings.js
+++ src/routings/Routings.js
@@ -6,6 +6,7 @@ const Routings = () => {
     <div>
         <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path='*' element={<Home/>} />
         </Routes>
     </div>
   )
